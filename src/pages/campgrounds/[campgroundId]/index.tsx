c7ee6b99ada00fe622c7b82c72b88e517c193f60
Pakorn Phongkhunakorn <pakorn22120@gmail.com>
2024-03-26 02:21:54 +0700

    Refactor campground page layout and add booking button

--- src/pages/campgrounds/[campgroundId]/index.tsx
+++ src/pages/campgrounds/[campgroundId]/index.tsx
@@ -17,31 +17,31 @@ export default function Campground() {
   const router = useRouter();
   const { campgroundId } = router.query;
 
-  const navigateToBooking = () => {
-    router.push(`${router.asPath}/booking`);
-  };
-
   return (
-    <div className="container mx-auto flex max-w-[800px] flex-col rounded-lg bg-gray-50 pt-16 shadow-md sm:flex-row">
-      <div className="relative h-96 w-80">
-        <Image
-          fill
-          src={CAMPGROUND_DATA.imageSrc}
-          alt="Alt"
-          className="object-cover"
-        />
+    <div className="flex h-screen items-center justify-center">
+      <div className="container flex max-w-[800px] flex-col gap-8 rounded-lg bg-gray-50 px-16 py-8 shadow-md sm:flex-row">
+        <div className="relative h-96 w-80">
+          <Image
+            fill
+            src={CAMPGROUND_DATA.imageSrc}
+            alt="Alt"
+            className="object-cover"
+          />
+        </div>
+        <div className="flex h-full flex-col items-start justify-start">
+          <h1 className="font-bold">{CAMPGROUND_DATA.name}</h1>
+          <p>{CAMPGROUND_DATA.address}</p>
+          <p>{CAMPGROUND_DATA.province}</p>
+          <p>{CAMPGROUND_DATA.postalcode}</p>
+          <p>{CAMPGROUND_DATA.tel}</p>
+          <p>{CAMPGROUND_DATA.region}</p>
+          <Link href={`/campgrounds/${campgroundId as string}/booking`}>
+            <button className="rounded-md bg-[#E9EB9E] px-4 py-2 hover:bg-[#ACC196]">
+              Book
+            </button>
+          </Link>
+        </div>
       </div>
-      <div>
-        <h1>{CAMPGROUND_DATA.name}</h1>
-        <p>{CAMPGROUND_DATA.address}</p>
-        <p>{CAMPGROUND_DATA.province}</p>
-        <p>{CAMPGROUND_DATA.postalcode}</p>
-        <p>{CAMPGROUND_DATA.tel}</p>
-        <p>{CAMPGROUND_DATA.region}</p>
-      </div>
-      <Link href={}>
-        <button>Book</button>
-      </Link>
     </div>
   );
 }
