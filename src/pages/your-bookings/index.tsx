376fab918ea2178ab43b4dd0fb32e91b599b0af9
LAPTOP-3BO2RVT9\acer120 <point640@gmail.com>
2024-03-26 21:59:31 +0700

    admin your booking

--- src/pages/your-bookings/index.tsx
+++ src/pages/your-bookings/index.tsx
@@ -2,13 +2,15 @@ import { useEffect, useState } from "react";
 import { api } from "~/utils/api";
 import { format } from "date-fns";
 import { type Booking } from "@prisma/client";
+import { useSession } from "next-auth/react";
 
 const BookingCard: React.FC<{ booking: Booking; campgroundname: string }> = ({
   booking,
   campgroundname,
 }) => {
+  const deleteBooking = api.user.cancelBooking.useMutation();
   return (
-    <div className="mb-4 rounded-lg bg-white p-6 shadow-md">
+    <div className="mb-4 rounded-lg bg-white p-6  shadow-md">
       <div className="mb-4 flex items-center justify-between">
         <h2 className="text-xl font-semibold">{campgroundname}</h2>
       </div>
@@ -23,7 +25,19 @@ const BookingCard: React.FC<{ booking: Booking; campgroundname: string }> = ({
         </p>
       </div>
       <div className="flex items-center justify-between">
-        <button className="rounded-md bg-red-600 px-4 py-2 text-white hover:bg-red-700">
+        <button
+          className="rounded-md bg-red-600 px-4 py-2 text-white hover:bg-red-700"
+          onClick={() =>
+            deleteBooking.mutate(
+              { bookingId: booking.id },
+              {
+                onSuccess: () => {
+                  window.location.reload();
+                },
+              },
+            )
+          }
+        >
           Cancel Booking
         </button>
       </div>
@@ -31,7 +45,40 @@ const BookingCard: React.FC<{ booking: Booking; campgroundname: string }> = ({
   );
 };
 
+const AdminBookingsPage: React.FC = () => {
+  const [bookings, setBookings] = useState<Booking[]>([]);
+  const { data: allBookings, isLoading } = api.admin.getAllBookings.useQuery();
+
+  useEffect(() => {
+    if (allBookings) {
+      setBookings(allBookings.bookings);
+    }
+  }, [allBookings]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="container mx-auto px-12 pt-24">
+      <h1 className="mb-8 text-3xl font-bold">All Bookings</h1>
+      {bookings.length === 0 ? (
+        <p>There are no bookings.</p>
+      ) : (
+        bookings.map((booking) => (
+          <BookingCard
+            key={booking.id}
+            booking={booking}
+            campgroundname={booking.campground.name}
+          />
+        ))
+      )}
+    </div>
+  );
+};
+
 const UserBookingsPage: React.FC = () => {
+  const { data: session } = useSession();
   const [bookings, setBookings] = useState<Booking[]>([]);
 
   const { data: userBookings, isLoading } =
@@ -48,7 +95,7 @@ const UserBookingsPage: React.FC = () => {
   }
 
   return (
-    <div className="container mx-auto py-8">
+    <div className="container mx-auto px-12 pt-24">
       <h1 className="mb-8 text-3xl font-bold">Your Bookings</h1>
       {bookings.length === 0 ? (
         <p>You have no bookings.</p>
@@ -65,4 +112,23 @@ const UserBookingsPage: React.FC = () => {
   );
 };
 
-export default UserBookingsPage;
+const BookingsPage: React.FC = () => {
+  const { data: session } = useSession();
+  const { data: user } = api.user.getCurrentUser.useQuery({});
+
+  if (user?.user?.role == "ADMIN") {
+    return (
+      <div>
+        <AdminBookingsPage />
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <UserBookingsPage />
+    </div>
+  );
+};
+
+export default BookingsPage;
