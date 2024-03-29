e8ed4da8a7252deb11c862150a011648980b41e7
Pakorn Phongkhunakorn <pakorn22120@gmail.com>
2024-03-29 21:08:29 +0700

    Change function name from Register to Login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { api } from "~/utils/api";
 
-export default function Register() {
+export default function Login() {
   const router = useRouter();
   const loginHandler = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
